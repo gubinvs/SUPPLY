a3d9da765b18f1b4d9c9df0195fc9051feb47405
master <gubinvs@gmail.com>
2024-03-12 21:18:11 +0300

    first commit

---
+++ Models/Company.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.SignalR;
+using SUPPLY;
+
+/// <summary>
+/// Структура с параметрами компании поставщика комплектующих
+/// </summary>
+/// yorCompany - Юридический статус компании (ИП, НПД, ООО, АО)
+/// nameCompany - Название компании
+/// innCompany - ИНН компании
+/// legalAddress - Юридический адрес компании
+struct Company {
+
+    public string yorCompany;
+    
+    public string nameCompany; 
+
+    public int innCompany;
+
+    public string legalAddress;
+    
+}  

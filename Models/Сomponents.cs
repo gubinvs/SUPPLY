5d52828b6c189be01b0b628766621c6504f3681a
master <gubinvs@gmail.com>
2024-03-13 19:22:17 +0300

    create component struct

--- Models/Сomponents.cs
+++ Models/Сomponents.cs
@@ -1,6 +1,17 @@
 using SUPPLY;
 
-
+/// <summary>
+/// Структура описывающая параметры комплектующиего
+/// </summary>
+/// vendorСode - арикул
+/// nameComponent - наименование
+/// manufacturer - производитель
 struct Components {
-    
+
+    public string vendorСode;
+
+    public string nameComponent;
+
+    public string manufacturer;
+
 }

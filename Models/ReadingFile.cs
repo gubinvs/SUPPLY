390383c6648c137ff8f7245429f6c0348919188a
master <gubinvs@gmail.com>
2024-03-14 14:34:13 +0300

    r

---
+++ Models/ReadingFile.cs
@@ -0,0 +1,21 @@
+using SUPPLY;
+using System.IO;
+
+
+
+/// <summary>
+///  Класс отвечающий за чтение информации из файла
+/// </summary>
+/// path - путь к файлу
+class ReadingFile {
+
+    // public string path = "/Users/vladimirgubin/web-developer/SUPPLY/wwwroot/file/component.csv";
+
+    public string path = "../wwwroot/file/component.csv";
+
+    using (FileStream component = new FileStream(path, ))
+    {
+        
+    }
+
+}

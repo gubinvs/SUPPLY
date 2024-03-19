5e2f2ca8f2abdce1022ce58165d11bdf2f22231d
master <gubinvs@gmail.com>
2024-03-19 11:43:14 +0300

    d

--- Models/ReadingExel.cs
+++ Models/ReadingExel.cs
@@ -1,20 +1,19 @@
 using System;
 using System.IO;
 using System.Text;
-using Microsoft.Office.Interop.Excel;
 using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
 using SUPPLY;
+
 
 
 /// <summary>
 ///  Класс отвечающий за чтение информации из файла
 /// </summary>
 /// path - путь к файлу
-class ReadingFile {
-
-    public string path = "/Users/vladimirgubin/web-developer/SUPPLY/wwwroot/file/component.xlsx";
-
-    Workbook workbook = new Workbook();
+class ReadingExel
+{
+    public Excel.Application excelapp = new Excel.Application();
 
 }
 

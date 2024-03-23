c96a71b3cf11e324675465c23edf3b6716554c9a
master <gubinvs@gmail.com>
2024-03-23 22:32:37 +0300

    sd

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,35 +2,35 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using SUPPLY.Models;
 
-namespace SUPPLY.Controllers {
+namespace SUPPLY.Controllers;
 
-    public class HomeController : Controller
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-
-            return View();
-        }
-    
-        public IActionResult Specification()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        _logger = logger;
     }
 
+    public IActionResult Index()
+    {
 
+        return View();
+    }
+
+    public IActionResult Specification()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
 }
+
+
+
 
